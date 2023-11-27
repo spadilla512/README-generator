6ee80bb8666bcece9b1266559133bfd05f90fc25
spadilla512 <spadilla512@gmail.com>
2023-11-27 17:38:40 -0600

    updated index.js file

--- index.js
+++ index.js
@@ -1,13 +1,99 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    //Name of project
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the name of your project?',
+    },
+    //Description
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Enter description about your project',
+    },
+    //Installation instructions
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Enter installation instructions to run your project',
+    },
+    //Usage information
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Enter usage information of your project repository',
+    },
+    //Contribution guidelines
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Enter guideline instructions on how to contribute to this project'
+    },
+    //test instructions
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Enter commands to use to run tests (if applicable)',
+    },
+    //License
+    {
+        ype: 'input',
+        name: 'license',
+        message: 'Select a license (if applicable) for your repository',
+        choices: ['Apache2.0', 'GNUGPv3.0', 'MIT', 'Mozilla Public License 2.0', 'the Unlicense' ]
+    },
+    //Your name
+    {
+        ype: 'input',
+        name: 'name',
+        message: 'What is your name?',
+    },
+    //github username
+    {
+        ype: 'input',
+        name: 'github',
+        message: 'Enter your GitHub Username',
+        
+    },
+    //link to github profile
+    {
+        type: 'input',
+        name: 'link',
+        message: 'Enter the link to your GitHub',
+    },
+    //email
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address',
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    return inquirer.createPromptModule(questions)
+        .then((data) => {
+            const markDown = generateMarkdown(data)
+            fs.writeFile('generated-README.md', markDown, function(err) {
+                if (err) {
+                    console.log('File could be created', err)
+                } else {
+                    console.log('Successfully created README file!')
+                }
+            })
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+}
 
 // Function call to initialize app
 init();
